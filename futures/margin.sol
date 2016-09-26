eea159b4eafb4b4e4d8ca466c6de438ed0297a62
Matthew Mills <mmil9732@uni.sydney.edu.au>
2016-09-26 14:35:21 +1000

    added to margin smart contract to include a deposit functionality

--- futures/margin.sol
+++ futures/margin.sol
@@ -16,7 +16,10 @@ contract Margin {
     uint public futureEnd;
 
     // Address of the owner of the margin account
-    uint public owner;
+    address public owner;
+
+    // Update the amount currently held in the margin account
+    uint public marginAmount;
 
     // Set to true at the end so as to disallow any changes and return any
     // remaining margin to the owner 
@@ -28,10 +31,40 @@ contract Margin {
 
     /// Create a margin contract with the associated future contract
     /// defined in the state variable futureContract
-    /// also define the start of the margin contract **Need to think this through***
-    function Margin(address _futureContract, uint _futureStart) {
+    /// also define the start of the margin contract **Need to think this through**
+    /// then links the margin to an owner account **Also need to think this through** 
+    function Margin(address _futureContract, uint _futureStart, 
+                   uint _futureEnd, address _owner) {
         futureContract = _futureContract;
         futureStart = _futureStart;
+        futureEnd = _futureEnd;
+        owner = _owner;
+        
+        // As Solidity is strictly typed, we must 0 initialise our amount in the 
+        // margin account upon contract creation
+        marginAmount = 0;
+    }
+
+    /// Increase the amount of Ether held by the margin account 
+    /// note that this is a cumulative action, hence calling this 
+    /// will continually increase the margin
+    function addMargin() payable {
+        // No need for arguments, as the amount to add is part of the transaction
+        // the keyword payable is required for the function to be allowed 
+        // to receive Ether
+
+        if (now > futureEnd) {
+            // Revert the call should the future contract have finished
+            // just in case for whatever reason the margin account is 
+            // still active - yay for error checking
+            throw;
+        }
+        // increase the amount of Ether held in the margin account by the value
+        // sent by the owner 
+        marginAmount = marginAmount + msg.value;
+                
+        // Log the increase
+        MarginIncrease(msg.sender, msg.value);
     }
 
 
