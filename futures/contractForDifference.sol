7276683ed686bdcd95ae6f61fdf3a6900922b930
Matthew Mills <mmil9732@uni.sydney.edu.au>
2016-10-23 01:54:55 +1100

    Finalised CFD to move to testing

--- futures/contractForDifference.sol
+++ futures/contractForDifference.sol
@@ -8,23 +8,101 @@ pragma solidity ^0.4.0;
  * value of the underlying security increases, A must pay B the difference and vice-versa
  */
 
+import "marginTwo.sol";
+import "priceTickerETHUSD.sol";
+
 contract ContractForDifference {
 
+    // Manage the balances of the margins
+    mapping (address => uint) private balance;
+
+    // Keep address of owner
+    address public owner;
+
     // Address for both short and long positions for the CFD
-    public address short;
-    public address long;
-
-    // Address for both short and long margin accounts for the CFD
-    private address shortMargin;
-    private address longMargin;
-
-    // Start and end times for the CFD
-    private uint startTime;
-    private uint endTime;
+    address[] private margins;
 
     // Price Ticker Value
-    public uint currETHUSD;
-    public uint prevETHUSD;
+    uint private prevETHUSD;
     
     // Address of Oracle
-    public address oracleETHUSD;
+    ETHUSDPriceTicker public oracleETHUSD;
+
+    // Check if terminated
+    bool isTerminated public;
+    // Events to indicate that the CFD has been marked
+    event CFDMarkShort(uint amount);
+    event CFDMarkLong(uint amount);
+    event updatedMargin(address accountAddress, uint amount);
+    
+    function ContractForDifference() {
+        owner = msg.sender;
+        margins.push(owner); 
+        oracleETHUSD = new ETHUSDPriceTicker();
+        prevETHUSD = oracleETHUSD.ETHUSD; 
+        isTerminated = false;
+    }
+
+    function deposit() public returns (uint) {
+        if(isTerminated) {
+            throw;
+        }
+        margins.push(msg.sender);
+        if (margins.length > 2){
+            throw;
+        }
+        balances[msg.sender] += msg.value;
+
+        updatedMargin(msg.sender, msg.value);
+        return balances[msg.sender];
+    }
+
+    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+        if(balances[msg.sender] >= withdrawAmount) {
+            balances[msg.sender] -= withdrawAmount;
+            
+            if (!msg.sender.send(withdrawAmount)) {
+                balances[msg.sender] += withdrawAmount;
+            }
+        }
+
+        return balances[msg.sender];
+    }
+
+    function mark(uint currPrice) public {
+        if (isTerminated) {
+            throw;
+        }
+        uint priceDiff = (currPrice - prevETHUSD);
+        
+        if (margins[0] <= priceDiff) {
+            liquidateShort();
+        }
+        else if (margins[1] <= priceDiff) {
+            liquidateLong();
+        }
+        else {
+            balances[margins[0]] -= priceDiff;
+            balances[margins[1]] += priceDiff;
+            prevETHUSD = currPrice;
+            updatedMargin(margins[0], (-1)*priceDiff);
+            updatedMargin(margins[1], priceDiff);
+        }
+    }
+
+    function liquidateShort() public {
+        balances[margins[1]] += balances[margins[0]];
+        balances[margins[0]] = 0;
+        isTerminated = true;
+    }
+
+    function liquidateLong() public {
+        balances[margins[0]] += balances[margins[1]];
+        balances[margins[1]] = 0;
+        isTerminated = true;
+        terminate();
+    }
+    
+    function () {
+        throw;
+    }
---
+++ futures/marginTwo.sol
@@ -0,0 +1,107 @@
+pragma solidity ^0.4.0;
+
+// Where one side of the futures contract deposits their "margin"
+// this contract then sends/receives the amount of Ethereum determined by the future
+// when it is marked to market
+    
+
+contract Margin {
+    // Address of the futures contract    
+    address public futureContract;
+    
+    // Parameters of the futures contract is synced with the margin 
+    // this offers a degree of error checking such that withdrawals and
+    // deposits cannot be made before or after the future is active 
+    uint public futureStart;
+    uint public futureEnd;
+
+    // Address of the owner of the margin account
+    address public owner;
+
+    // Update the amount currently held in the margin account
+    uint public marginAmount;
+
+    // Set to true at the end so as to disallow any changes and return any
+    // remaining margin to the owner 
+    bool ended;
+
+    // Events that will be fired on changes
+    event MarginIncrease(uint amount);
+    event FutureClosed(uint amount);
+
+    /// Create a margin contract with the associated future contract
+    /// defined in the state variable futureContract
+    /// also define the start of the margin contract **Need to think this through**
+    /// then links the margin to an owner account **Also need to think this through** 
+    function Margin(uint _futureStart, 
+                   uint _futureEnd, address _owner) {
+        futureStart = _futureStart;
+        futureEnd = _futureEnd;
+        owner = _owner;
+        
+        // As Solidity is strictly typed, we must 0 initialise our amount in the 
+        // margin account upon contract creation
+        marginAmount = 0;
+    }
+
+    /// Increase the amount of Ether held by the margin account 
+    /// note that this is a cumulative action, hence calling this 
+    /// will continually increase the margin
+    function deposit() payable {
+        // No need for arguments, as the amount to add is part of the transaction
+        // the keyword payable is required for the function to be allowed 
+        // to receive Ether
+
+        if (now > futureEnd) {
+            // Revert the call should the future contract have finished
+            // just in case for whatever reason the margin account is 
+            // still active - yay for error checking
+            throw;
+        }
+        // increase the amount of Ether held in the margin account by the value
+        // sent by the owner 
+        marginAmount = marginAmount + msg.value;
+                
+        // Log the increase
+        MarginIncrease(msg.value);
+    }
+    
+    // Withdraw from the margin account 
+    // Need to account for the liquidity minimums as well as how to do margin calls
+    function withdraw() returns (bool) {
+        var amount = msg.sender;
+        if (amount > 0) {
+            if (!msg.sender.send(msg.value)) {
+                return false;
+            }
+            return true;
+        }
+    }
+    /// End the margin account due expiry of future contract
+    function closeMargin() {
+        // 1. Checking conditions
+        if (now <= futureEnd)
+            throw; // margin being closed too early
+        if (ended)
+            throw; // margin already closed
+
+        // 2. Effects
+        ended = true;
+        FutureClosed(marginAmount);
+
+        // 3. Interaction
+        if (!owner.send(marginAmount))
+            throw;
+    }
+
+    /// Forced liquidation of the margin account due to a margin call
+    function liquidateMargin() {
+        if (ended)
+            throw;
+        ended = true;
+        FutureClosed(marginAmount);
+
+        if (!futureContract.send(marginAmount))
+            throw;
+    }
+}
